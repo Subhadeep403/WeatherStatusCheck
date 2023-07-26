0722d8ebfab6511c1b9d50b0debd4f9d77952679
SUBHADEEP DAS <subhadeepdas950@gmail.com>
2023-07-26 08:45:00 +0530

    Update WeatherCheck.java

--- src/test/java/Test/WeatherCheck.java
+++ src/test/java/Test/WeatherCheck.java
@@ -5,7 +5,11 @@
 	import io.restassured.path.json.JsonPath;
 	import io.restassured.response.Response;
 	import io.restassured.specification.RequestSpecification;
-
+/**
+ * 
+ * @author subhadeep
+ *
+ */
 public class WeatherCheck {
 
 	
